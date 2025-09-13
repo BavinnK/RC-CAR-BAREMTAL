8f0a151377a48c243b7b6b431b0578da0cf09af5
Bavin <bavinbav5@gmail.com>
2025-09-14 00:52:35 +0300

    now everything is working beautifully loooook at this masterpieace hahaha

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <avr/wdt.h>
 #include <Arduino.h>
 #include <avr/sleep.h>
+#include <MyUSART.h>
+#define clk_speed 16000000
+#define baud 9600
+#define my_ubrr (clk_speed/16/baud-1)
 enum L298N_pins{
   For_dir_btn= PC0,
   Rev_dir_btn= PC1,
@@ -38,20 +42,21 @@ void set_PCINT(void){
 volatile long int milisecond=0;
 ISR(PCINT1_vect){
   milisecond=0;
-  //we have no code here why? bc when we put the mcu to deep sleep only some parameters can wake the mcu up we only reset the counter in the ISR thats it
+  // when we put the mcu to deep sleep only some parameters can wake the mcu up we only reset the counter in the ISR thats it
   //1-watchdog INT
   //2-external INTS 0 and 1
   //3-PINchange INTS we chose the pcints to wake the mcu up so we dont put anycode in the routine
 }
 ISR(TIMER2_OVF_vect){
   milisecond++;
+  //every 1.024ms this ISR will accour and inrements the milisecond variable
 }
 void deep_sleep(void) {
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
     sleep_cpu();
 }
-//in this function we set timer 2 INT so each 1024 us it will send an intterrupt or we can say each 1ms overflow happens and sends an intterrupt
+//in this function we set timer 2 INT so each 1024 us or 1.024ms it will send an intterrupt or we can say each 1ms overflow happens and sends an intterrupt
 
 
 void set_timer2(void){
@@ -63,16 +68,18 @@ void setup(){
   set_PCINT();
   watchdog_init();
   set_timer2();
+  USART_init(my_ubrr);
   DDRC&=~((1<<For_dir_btn)|(1<<Rev_dir_btn)|(1<<Lef_dir_btn)|(1<<Rig_dir_btn));
   PORTC|=(1<<For_dir_btn)|(1<<Rev_dir_btn)|(1<<Lef_dir_btn)|(1<<Rig_dir_btn);
   DDRB|=((1<<Enb_A)|(1<<Enb_B));
   // PORTB|=(1<<Enb_A)|(1<<Enb_B); just set them HIGH means full speed low means zero 127 means hald speed cuz it from 0 to 255
-  //
+  
   DDRD|=(1<<IN1)|(1<<IN2)|(1<<IN3)|(1<<IN4);
   set_PWM();
   ENA_speed(127);
   ENB_speed(127);
   sei();
+  USART_strTransmit("MCU starting .....\n1\n2\n3\nwatchdog is enabled!\n");
 
 }
 
@@ -82,34 +89,44 @@ void loop() {
   bool btnRe_pressed=!(PINC&(1<<Rev_dir_btn));
   bool btnLe_pressed=!(PINC&(1<<Lef_dir_btn));
   bool btnRi_pressed=!(PINC&(1<<Rig_dir_btn));
+  
   if(btnFo_pressed){
     PORTD|=(1<<IN2)|(1<<IN4);
     PORTD&=~((1<<IN1)|(1<<IN3));
-    wdt_reset();
+    
+     USART_strTransmit("drone going forward\n");
     
   }
   else if(btnRe_pressed){
     PORTD|=(1<<IN1)|(1<<IN3);
     PORTD&=~((1<<IN2)|(1<<IN4));
-    wdt_reset();
+    
+    USART_strTransmit("drone going reverse\n");
   }
   else if(btnLe_pressed){
     PORTD|=(1<<IN1)|(1<<IN4);
     PORTD&=~((1<<IN2)|(1<<IN3));
-    wdt_reset();
+    
+    USART_strTransmit("drone going left\n");
   }
   else if(btnRi_pressed){
     PORTD|=(1<<IN2)|(1<<IN3);
     PORTD&=~((1<<IN1)|(1<<IN4));
-    wdt_reset();
+    
+    USART_strTransmit("drone going right\n");
   }
   else {
     PORTD&=~((1<<IN2)|(1<<IN1)|(1<<IN3)|(1<<IN4));
     
     wdt_reset();
   }
+  //if after 50 second non of the buttons were pressed the mcu goes into a deep sleep whenever any of the button pressen and ISR will accour and wakes the MCU up again after 50 sec
+  //if no buttons were used it will go back to sleep 
   if(milisecond>=50000){
+     USART_strTransmit("MCU going into deep sleep\n");
+     wdt_disable();
     deep_sleep();
+    wdt_enable(WDTO_8S);
   }
 }
 
